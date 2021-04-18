8632520d147af4e262a6c2736b524d963d610a50
noionion <noionion@outlook.com>
2021-04-18 11:21:04 +0800

    update

--- AutoClockIn.py
+++ AutoClockIn.py
@@ -4,35 +4,47 @@ import json
 import os
 
 headers_1 = {
-    "Cookie": "arccount62298=c; arccount62019=c",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66"
-}
+        "Cookie": "arccount62298=c; arccount62019=c",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66"
+    } # 验证码token爬取
 
 headers_2 = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "Connection": "keep-alive",
-    "Content-Type": "application/json;charset=UTF-8",
-    "Host": "fangkong.hnu.edu.cn",
-    "Origin": "https://fangkong.hnu.edu.cn",
-    "Referer": "https://fangkong.hnu.edu.cn/app/",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75"
-}
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json;charset=UTF-8",
+        "Host": "fangkong.hnu.edu.cn",
+        "Origin": "https://fangkong.hnu.edu.cn",
+        "Referer": "https://fangkong.hnu.edu.cn/app/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75"
+    } # 登录及打卡
+
+headers_3 = {
+        'Host': 'cloud.baidu.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36 Edg/89.0.774.76',
+        'Accept': '*/*',
+        'Origin': 'https://cloud.baidu.com',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://cloud.baidu.com/product/ocr/general',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    } # 百度OCR识别验证码
 
 # 获取变量
 
 usr = os.getenv("usr")
 pwd = os.getenv("pwd")
-sckey = os.getenv("sckey")
 RealAddress = os.getenv("RealAddress")
 l = os.getenv("RealProvince_City_County").split(",")
 RealCity = l[1]
 RealCounty = l[2]
 RealProvince = l[0]
+
 
 # step 1: 获取验证码Token及图片
 
@@ -46,19 +58,25 @@ def ClockIn():
 
         data = json.loads(token_json.text)
         token = data["data"]["Token"]
-
+        
         img_url = "https://fangkong.hnu.edu.cn/imagevcode?token=" + token
         with open("img.jpg", "wb") as img:
             img.write(requests.get(img_url).content)
 
+        
         # 解析验证码
 
         with open("img.jpg",'rb') as f:
             img = base64.b64encode(f.read())
-        data = '{"image":"%s"}'%str(img)[2:]
+        data = {
+            'image': 'data:image/jpeg;base64,'+str(img)[2:-1],
+            'image_url': '',
+            'type': 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic',
+            'detect_direction': 'false'
+        }
 
-        response = requests.post('https://www.paddlepaddle.org.cn/paddlehub-api/image_classification/chinese_ocr_db_crnn_mobile', headers=headers_1, data=data)
-        result = response.json()['result'][0]["data"][0]["text"]
+        response = requests.post('https://cloud.baidu.com/aidemo', headers=headers_3, data=data)
+        result = response.json()['data']['words_result'][0]['words']
 
         # step 2: 模拟登录操作
 
@@ -74,7 +92,6 @@ def ClockIn():
 
         if response.json()["code"] != 0:
             print("验证码错误")
-            # requests.get("http://sc.ftqq.com/"+ sckey +".send?text=打卡失败啦&desp=验证码错误，正在重试")
             ClockIn()
         else:
             # step 3: 模拟打卡操作
@@ -130,10 +147,8 @@ def ClockIn():
 
             msg = response.json()["msg"]
             print(msg)
-            # requests.get("http://sc.ftqq.com/"+ sckey +".send?text=打卡成功啦！&desp=" + msg)
     except:
         print("Error")
-        # requests.get("http://sc.ftqq.com/"+ sckey +".send?text=打卡失败啦！&desp=打卡失败，正在重试")
         ClockIn()
 
 if __name__ == '__main__':
